8de6acc13bf49c60c1e238a7fa8eb21f2905b5e1
nguyenxuanson1102 <xuansonn1102@gmail.com>
2020-08-06 09:56:10 +0700

    Revert "add exe4"
    This reverts commit 36756052c4708f03af49e4c0b4fedba98f838bf6.

---
+++ main/java/com/example/zingmp3/Song.java
@@ -0,0 +1,30 @@
+package com.example.zingmp3;
+
+public class Song {
+    public Song(String name, int file) {
+        this.name = name;
+        this.file = file;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFile() {
+        return file;
+    }
+
+    public void setFile(int file) {
+        this.file = file;
+    }
+
+    private String name;
+    private int file;
+
+
+
+}
